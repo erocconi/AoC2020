0fb1c9a42cf1fd0b94afa1b667d96f3905acabcc
Eric Rocconi <eric.rocconi@sap.com>
2020-12-03 17:07:50 -0800

    f strings
    undescriptive

--- d1.py
+++ d1.py
@@ -5,7 +5,7 @@ with open("./input/d1.txt", "r") as inputFile:
     for item1 in input:
         for item2 in input:
             if (item1 + item2) == 2020:
-                print("Part 1 == {}".format(item1*item2))
+                print(f'Part 1: {(item1*item2)}')
             for item3 in input:
                 if (item1 + item2 + item3) == 2020:
-                    print("Part 2 == {}".format(item1*item2*item3))
+                    print(f'Part 2: {(item1*item2*item3)}')
